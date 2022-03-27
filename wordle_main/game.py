8ae96303fefcfa8bc7594456c91a9987f86fbe74
edoatley <edoatley@gmail.com>
2022-03-27 10:29:30 +0100

    fix formatting issue

--- wordle_main/game.py
+++ wordle_main/game.py
@@ -69,5 +69,5 @@ class Game:
 
 
 if __name__ == "__main__":
-    game = Game("/Users/edoatley/Personal/python-wordle/wordle_main/dictionary.txt")
+    game = Game("wordle_main/dictionary.txt")
     game.game_loop()
